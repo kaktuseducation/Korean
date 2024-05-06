10009b9268236974f37cebe0372179b0170e1460
kaktus education <130348932+kaktuseducation@users.noreply.github.com>
2024-05-06 20:02:18 +0500

    Update dictionary.js

--- JS/dictionary.js
+++ JS/dictionary.js
@@ -1,78 +1,178 @@
 LEVELS = {
   'Level 1' : {
-    'chapters' : ['alphabet', 'chapter 1', 'chapter 2', 'chapter 3', 'chapter 4', 'chapter 5', 'chapter 6', 'chapter 7', 'chapter 8'],
-    'alphabet' : ['lesson 1', 'lesson 2', 'lesson 3'],
-    'chapter 1' : [],
-    'chapter 2' : [],
-    'chapter 3' : [],
-    'chapter 4' : [],
-    'chapter 5' : [],
-    'chapter 6' : [],
-    'chapter 7' : [],
-    'chapter 8' : [],
+    'alphabet' : {
+      "name" : "alphabet"      
+    },
+    'chapter 1' : {
+      "name" : "chapter 1"
+    },
+    'chapter 2' : {
+      "name" : "chapter 2"
+    },
+    'chapter 3' : {
+      "name" : "chapter 3"
+    },
+    'chapter 4' : {
+      "name" : "chapter 4"
+    },
+    'chapter 5' : {
+      "name" : "chapter 5"
+    },
+    'chapter 6' : {
+      "name" : "chapter 6"
+    },
+    'chapter 7' : {
+      "name" : "chapter 7"
+    },
+    'chapter 8' : {
+      "name" : "chapter 8"
+    },
   },
     
-    'Level 2' : {
-    'chapters' : [ 'chapter 9', 'chapter 10', 'chapter 11', 'chapter 12', 'chapter 13', 'chapter 14', 'chapter 15', 'chapter 16'],
-    'chapter 9' : [],
-    'chapter 10' : [],
-    'chapter 11' : [],
-    'chapter 12' : [],
-    'chapter 13' : [],
-    'chapter 14' : [],
-    'chapter 15' : [],
-    'chapter 16' : [],
+  'Level 2' : {
+    'chapter 9' : {
+      "name" : "chapter 9"
+    },
+    'chapter 10' : {
+      "name" : "chapter 10"
+    },
+    'chapter 11' : {
+      "name" : "chapter 11"
+    },
+    'chapter 12' : {
+      "name" : "chapter 12"
+    },
+    'chapter 13' : {
+      "name" : "chapter 13"
+    },
+    'chapter 14' : {
+      "name" : "chapter 14"
+    },
+    'chapter 15' : {
+      "name" : "chapter 15"
+    },
+    'chapter 16' : {
+      "name" : "chapter 16"
+    },
+    'chapter 17' : {
+      "name" : "chapter 17"
+    },
   },
 
   'Level 3' : {
-    'chapters' : ['chapter 1', 'chapter 2', 'chapter 3', 'chapter 4', 'chapter 5', 'chapter 6', 'chapter 7', 'chapter 8', 'chapter 9'],
-    'chapter 1' : [],
-    'chapter 2' : [],
-    'chapter 3' : [],
-    'chapter 4' : [],
-    'chapter 5' : [],
-    'chapter 6' : [],
-    'chapter 7' : [],
-    'chapter 8' : [],
-    'chapter 9' : [],
+    'chapter 18' : {
+      "name" : "chapter 18"
+    },
+    'chapter 19' : {
+      "name" : "chapter 19"
+    },
+    'chapter 20' : {
+      "name" : "chapter 20"
+    },
+    'chapter 21' : {
+      "name" : "chapter 21"
+    },
+    'chapter 22' : {
+      "name" : "chapter 22"
+    },
+    'chapter 23' : {
+      "name" : "chapter 23"
+    },
+    'chapter 24' : {
+      "name" : "chapter 24"
+    },
+    'chapter 25' : {
+      "name" : "chapter 25"
+    },
+    'chapter 26' : {
+      "name" : "chapter 26"
+    },
   },
 
   'Level 4' : {
-    'chapters' : ['chapter 10', 'chapter 11', 'chapter 12', 'chapter 13', 'chapter 14', 'chapter 15', 'chapter 16', 'chapter 17', 'chapter 18'],
-    'chapter 10' : [],
-    'chapter 11' : [],
-    'chapter 12' : [],
-    'chapter 13' : [],
-    'chapter 14' : [],
-    'chapter 15' : [],
-    'chapter 16' : [],
-    'chapter 17' : [],
-    'chapter 18' : [],
+    'chapter 27' : {
+      "name" : "chapter 27"
+    },
+    'chapter 28' : {
+      "name" : "chapter 28"
+    },
+    'chapter 29' : {
+      "name" : "chapter 29"
+    },
+    'chapter 30' : {
+      "name" : "chapter 30"
+    },
+    'chapter 31' : {
+      "name" : "chapter 31"
+    },
+    'chapter 32' : {
+      "name" : "chapter 32"
+    },
+    'chapter 33' : {
+      "name" : "chapter 33"
+    },
+    'chapter 34' : {
+      "name" : "chapter 34"
+    },
+    'chapter 35' : {
+      "name" : "chapter 35"
+    },
   },
 
   'Level 5' : {
-    'chapters' : ['chapter 1', 'chapter 2', 'chapter 3', 'chapter 4', 'chapter 5', 'chapter 6', 'chapter 7', 'chapter 8', 'chapter 9'],
-    'chapter 1' : [],
-    'chapter 2' : [],
-    'chapter 3' : [],
-    'chapter 4' : [],
-    'chapter 5' : [],
-    'chapter 6' : [],
-    'chapter 7' : [],
-    'chapter 8' : [],
-    'chapter 9' : [],
+    'chapter 36' : {
+      "name" : "chapter 36"
+    },
+    'chapter 37' : {
+      "name" : "chapter 37"
+    },
+    'chapter 38' : {
+      "name" : "chapter 38"
+    },
+    'chapter 39' : {
+      "name" : "chapter 39"
+    },
+    'chapter 40' : {
+      "name" : "chapter 40"
+    },
+    'chapter 41' : {
+      "name" : "chapter 41"
+    },
+    'chapter 42' : {
+      "name" : "chapter 42"
+    },
+    'chapter 43' : {
+      "name" : "chapter 43"
+    },
+    'chapter 44' : {
+      "name" : "chapter 44"
+    },
   },
 
   'Level 6' : {
-    'chapters' :  ['chapter 10', 'chapter 11', 'chapter 12', 'chapter 13', 'chapter 14', 'chapter 15', 'chapter 16', 'chapter 17', 'chapter 18'],
-    'chapter 10' : [],
-    'chapter 11' : [],
-    'chapter 12' : [],
-    'chapter 13' : [],
-    'chapter 14' : [],
-    'chapter 15' : [],
-    'chapter 16' : [],
-    'chapter 17' : [],
-    'chapter 18' : [],
+    'chapter 45' : {
+      "name" : "chapter 45"
+    },
+    'chapter 46' : {
+      "name" : "chapter 46"
+    },
+    'chapter 47' : {
+      "name" : "chapter 47"
+    },
+    'chapter 48' : {
+      "name" : "chapter 48"
+    },
+    'chapter 49' : {
+      "name" : "chapter 49"
+    },
+    'chapter 50' : {
+      "name" : "chapter 50"
+    },
+    'chapter 51' : {
+      "name" : "chapter 51"
+    },
+    'chapter 52' : {
+      "name" : "chapter 52"
+    },
   },
 }
